4e94aa6cb7eeb2902295b7a2d8cd5f9343ecb5d6
Maureen Depresle <46751529+maureen69170@users.noreply.github.com>
2020-06-12 05:40:43 +0200

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,23 @@
+<?php
+include 'partials/nav.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home page</title>
+</head>
+<body>
+<header>
+   <h1>Welcome on Board</h1>
+   <h4>I want to be notified</h4>
+   <img src="assets/images/header-img.jpg" alt="">
+   <form action="">
+   <input type="text" placeholder="Enter your address mail" class="txt-input">
+   <input type="submit" value="Send" class="">
+   </form>
+</header>
+</body>
+</html>
+
